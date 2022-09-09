c945a9e729aa011ae06bab42f5453181e76719ef
Lee dong ho <dong963939@gmail.com>
2022-09-09 23:51:55 +0900

    ♻️ index.html 삭제 및 server.js 수정함.

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const path = require('path');
-// const cors = require('cors');
 const app = express();
-
-const server = require('http').createServer(app);
-
-// app.use(cors()); // cors 미들웨어를 삽입합니다.
-
 app.use(express.json());
 var cors = require('cors');
 app.use(cors());
+
+const server = require('http').createServer(app);
 
 server.listen(8080, () => {
     console.log('server is running on 8080')
@@ -24,12 +20,3 @@ app.get('/', function (req, res) {
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '/front/dist/index.html'));
 });
-
-// const express = require('express');
-// const path = require('path');
-// const cors = require('cors');
-// const app = express();
-
-// const server = require('http').createServer(app);
-
-// app.use(cors()); // cors 미들웨어를 삽입합니다.
